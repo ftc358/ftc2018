588cc7b063bda91235b6aa8884249b6048c76b9f
Lawrence <DenouementX@users.noreply.github.com>
2018-09-12 16:11:56 -0700

    version control test

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
@@ -50,6 +50,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
+//test test test
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
 @Disabled
 public class BasicOpMode_Iterative extends OpMode
