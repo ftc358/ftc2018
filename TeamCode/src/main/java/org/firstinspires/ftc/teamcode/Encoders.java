3da5af7041b6b39bb71b222dd439ddce8c64fa79
Lawrence <DenouementX@users.noreply.github.com>
2018-09-13 16:21:31 -0700

    updates. unknown issue while testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
@@ -1,11 +1,7 @@
-package org.firstinspires.ftc.teamcode;
 // autonomous program that drives bot forward a set distance, stops then
 // backs up to the starting point using encoders to measure the distance.
-// ANDYMARK_TICKS_PER_REV = 1120
 
-/**
- * Created by lawrencemao on 9/6/18.
- */
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -20,11 +16,11 @@ public class Encoders extends LinearOpMode
     @Override
     public void runOpMode() throws InterruptedException
     {
-        leftMotor = hardwareMap.dcMotor.get("leftMotor");
-        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        leftMotor = hardwareMap.dcMotor.get("left_motor");
+        rightMotor = hardwareMap.dcMotor.get("right_motor");
         rightMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        // reset encoder count kept by motors.
+        // reset encoder count
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -42,23 +38,19 @@ public class Encoders extends LinearOpMode
         telemetry.addData("Mode", "running");
         telemetry.update();
 
-        // set motors to run for 5000 encoder counts.
+        // set left motor to run for 5000 encoder counts.
 
         leftMotor.setTargetPosition(-5000);
-        rightMotor.setTargetPosition(-5000);
 
         // set both motors to 25% power. Movement will start.
 
         leftMotor.setPower(-0.25);
         rightMotor.setPower(-0.25);
 
-        // wait while opmode is active and motors are busy running to position.
+        // wait while opmode is active and left motor is busy running to position.
 
         while (opModeIsActive() && leftMotor.isBusy() && rightMotor.isBusy())
         {
-            telemetry.addData("encoder-fwd", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
-            telemetry.addData("encoder-fwd", rightMotor.getCurrentPosition() + "  busy=" + rightMotor.isBusy());
-            telemetry.update();
             idle();
         }
 
@@ -74,8 +66,6 @@ public class Encoders extends LinearOpMode
 
         while (opModeIsActive() && getRuntime() < 5)
         {
-            telemetry.addData("encoder-fwd-end", leftMotor.getCurrentPosition() + "  busy=" + leftMotor.isBusy());
-            telemetry.update();
             idle();
         }
 
@@ -91,18 +81,15 @@ public class Encoders extends LinearOpMode
         leftMotor.setPower(0.25);
         rightMotor.setPower(0.25);
 
+        while (opModeIsActive() && leftMotor.getCurrentPosition() < leftMotor.getTargetPosition())
+        {
+            idle();
+        }
+
         // set motor power to zero to stop motors.
 
         leftMotor.setPower(0.0);
         rightMotor.setPower(0.0);
 
-        resetStartTime();
-
-        while (opModeIsActive() && getRuntime() < 5)
-        {
-            telemetry.addData("encoder-back-end", leftMotor.getCurrentPosition());
-            telemetry.update();
-            idle();
-        }
     }
 }
