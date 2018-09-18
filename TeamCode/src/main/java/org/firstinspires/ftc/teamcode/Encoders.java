1070be83e8eafe6d224e5eb3554efec87462053e
Lawrence <DenouementX@users.noreply.github.com>
2018-09-17 17:01:59 -0700

    encoder library and sample autonomous to play the pre-game (using library)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoders.java
@@ -1,96 +1,67 @@
-// autonomous program that drives bot forward a set distance, stops then
-// backs up to the starting point using encoders to measure the distance.
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous
-public class Encoders extends LinearOpMode
-{
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+public class Encoders {
 
-    @Override
-    public void runOpMode() throws InterruptedException
-    {
-        leftMotor = hardwareMap.dcMotor.get("left_motor");
-        rightMotor = hardwareMap.dcMotor.get("right_motor");
-        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+    public static void Forward (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
 
-        // reset encoder count
+        //Reset Encoders
         leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        // set motors to run to target encoder position and stop with brakes on.
+        //Set to RUN_TO_POSITION mode
         leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        telemetry.addData("Mode", "waiting");
-        telemetry.update();
+        //Set Target Position
+        leftMotor.setTargetPosition(distance);
+        rightMotor.setTargetPosition(distance);
 
-        // wait for start button.
+        //Set Drive Power
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
 
-        waitForStart();
-
-        telemetry.addData("Mode", "running");
-        telemetry.update();
-
-        // set left motor to run for 5000 encoder counts.
-
-        leftMotor.setTargetPosition(-5000);
-        rightMotor.setTargetPosition(-5000);
-
-        // set both motors to 25% power. Movement will start.
-
-        leftMotor.setPower(-0.25);
-        rightMotor.setPower(-0.25);
-
-        // wait while opmode is active and left motor is busy running to position.
-
-        while (opModeIsActive() && leftMotor.isBusy() && rightMotor.isBusy())
+        while (leftMotor.isBusy() && rightMotor.isBusy())
         {
-            idle();
+            //Wait Until Target Position is Reached
         }
 
-        // set motor power to zero to turn off motors. The motors stop on their own but
-        // power is still applied so we turn off the power.
-
-        leftMotor.setPower(0.0);
-        rightMotor.setPower(0.0);
-
-        // wait 5 sec to you can observe the final encoder position.
-
-        resetStartTime();
-
-        while (opModeIsActive() && getRuntime() < 5)
-        {
-            idle();
-        }
-
-        // set position for back up to starting point. In this example instead of
-        // having the motor monitor the encoder we will monitor the encoder ourselves.
-
-        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        leftMotor.setTargetPosition(0);
-        rightMotor.setTargetPosition(0);
-
-        leftMotor.setPower(0.25);
-        rightMotor.setPower(0.25);
-
-        while (opModeIsActive() && leftMotor.getCurrentPosition() < leftMotor.getTargetPosition())
-        {
-            idle();
-        }
-
-        // set motor power to zero to stop motors.
-
-        leftMotor.setPower(0.0);
-        rightMotor.setPower(0.0);
+        //Stop and Change Mode back to Normal
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
 
     }
+
+    //negative distance to turn left; positive distance to turn right
+
+    public static void Turn (DcMotor leftMotor, DcMotor rightMotor, double power, int distance) {
+
+        //Reset Encoders
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //Set to RUN_TO_POSITION mode
+        leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //Set Target Position
+        leftMotor.setTargetPosition(distance);
+        rightMotor.setTargetPosition(-1*distance);
+
+        //Set Drive Power
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+
+        while (leftMotor.isBusy() && rightMotor.isBusy())
+        {
+            //Wait Until Target Position is Reached
+        }
+
+        //Stop and Change Mode back to Normal
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+
+    }
+
 }
