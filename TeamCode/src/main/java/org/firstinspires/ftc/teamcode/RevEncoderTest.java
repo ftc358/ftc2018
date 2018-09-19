2952a95c9de7b8101ce26aa3e5c1071db635c138
kevinwang <PhosphorusElement15@gmail.com>
2018-09-19 16:44:15 -0700

    :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevEncoderTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.hardware.
+import com.qualcomm.robotcore.hardware.HardwareDevice;
 
 @Autonomous
 
@@ -14,7 +14,7 @@ public class RevEncoderTest extends LinearOpMode{
 
     public void runOpMode() throws InterruptedException {
 
-        Motor1 = hardwareMap.get("Motor1");
+        Motor1 = hardwareMap.dcMotor.get("Motor1");
         Motor2 = hardwareMap.dcMotor.get("Motor2");
 
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleAutonomous.java
@@ -23,7 +23,6 @@ public class SampleAutonomous extends LinearOpMode
         // wait for start button.
         waitForStart();
 
-
         Encoders.Forward(leftMotor, rightMotor,0.25, 1000);
         Encoders.Turn(leftMotor, rightMotor,0.25, 450);
     }
